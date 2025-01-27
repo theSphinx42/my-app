9aff659acf6638156b98f8c52ebaa80fac2658eb
Sphinx <91190033+theSphinx42@users.noreply.github.com>
2025-01-27 04:50:41 -0800

    Create input.js

---
+++ src/compnents/ui/input.js
@@ -0,0 +1,9 @@
+// Get user input using prompt()
+const userName = prompt("Please enter your name:");
+
+// Display the input back to the user
+if (userName) {
+  alert("Hello, " + userName + "!");
+} else {
+  alert("You didn't enter a name.");
+}
